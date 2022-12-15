bb02b500bffecb05069ef5eef95d42b4be3bc2df
nathanmeade <nathan.r.meade@gmail.com>
2022-12-15 07:41:09 -0500

    Basic Retrofit call with a logging statement along with a sample navigation graph with fragments implemented.

--- app/src/main/java/com/haigsoftware/uinetworknavigationtestapp/MainActivity.kt
+++ app/src/main/java/com/haigsoftware/uinetworknavigationtestapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.haigsoftware.uinetworknavigationtestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.fragment.NavHostFragment
+import com.haigsoftware.uinetworknavigationtestapp.network.QuotesApi
+import com.haigsoftware.uinetworknavigationtestapp.network.RetrofitHelper
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quotesApi = RetrofitHelper.getInstance().create(QuotesApi::class.java)
+
+        GlobalScope.launch {
+            val result = quotesApi.getQuotes()
+                if (result != null)
+                    Log.d("ayush: ", result.body().toString())
+        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
+    
 }
